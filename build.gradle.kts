d40109cd91379e4563d147ec66606aff58ed0b04
Cazzstudios Enterprise <cazzstudios@gmail.com>
2026-07-02 11:15:59 +0200

    ci(deps): add Dependabot config and Gradle dependency locking

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,13 @@ plugins {
     id("io.gitlab.arturbosch.detekt") version "1.23.7" apply false
     id("org.jlleitschuh.gradle.ktlint") version "12.1.1" apply false
 }
+
+dependencyLocking {
+    lockAllConfigurations()
+}
+
+subprojects {
+    dependencyLocking {
+        lockAllConfigurations()
+    }
+}
